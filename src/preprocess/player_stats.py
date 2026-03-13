86d7826feaf933e0c46e9bb012e3b091bff57525
lancehendricks <hendricks.la@northeastern.edu>
2026-03-12 20:51:03 -0400

    Changed data to aggregate over games

--- src/preprocess/player_stats.py
+++ src/preprocess/player_stats.py
@@ -20,11 +20,16 @@ def get_player_ids(understat: UnderstatClient, positions: Set[str], league: str 
     pos_players = list(filter(lambda p: any(pp in positions for pp in p["position"].split(" ")), players))
     return [p["id"] for p in pos_players]
     
-def get_player_stats_df(understat: UnderstatClient, player_id: str,
+def get_player_stats_df(understat: UnderstatClient, player_id: str, games_per_block: int,
                       stats: List[str]) -> pd.DataFrame:
     """
-    Produces a dataframe with per-90 stats for every game played by the given
+    Produces a dataframe with aggregate per-90 stats for every games_per_block games played by the given
     player_id, for any club or season played. Indexes by date.
+    
+    Note each row contains data for a disjoint set of games.
+    
+    Note if the player played less than games_per_block games, an empty dataframe
+    will be returned.
     """
     # Get all player matches
     player_matches = understat.player(player=player_id).get_match_data()
@@ -34,27 +39,45 @@ def get_player_stats_df(understat: UnderstatClient, player_id: str,
         
     # Sort by date
     player_matches_df = player_matches_df.sort_values(by="date")
-        
+    
     # Per-90 stats over games
     per_90_stats = list(map(lambda s: f"{s}_per_90", stats))
-
-    # Stats/90 for each
-    for stat, per_90_stat in zip(stats, per_90_stats):
-        player_matches_df[per_90_stat] = player_matches_df[stat].astype(float) / player_matches_df["time"].astype(int) * 90
-
-    # Only date plus stats
-    stats_df = player_matches_df[["date"] + per_90_stats].copy()
     
-    # Set index to id and date
-    stats_df = stats_df.set_index("date")
+    def aggregate_in_window(window_df):
+        """Constructs a particular aggregated row with per-90 stats."""
+        mins = window_df["time"].astype(int).sum()
         
-    return stats_df
+        row = {}
+        
+        for stat, per_90_stat in zip(stats, per_90_stats):
+            row[per_90_stat] = window_df[stat].astype(float).sum() / mins * 90
+        
+        # For date, take the max
+        row["date"] = max(window_df["date"])
+            
+        return row
+        
+    # Construct aggregated_df
+    rows = []
+    
+    for i in range(0, len(player_matches_df) - games_per_block, games_per_block):
+        rows.append(aggregate_in_window(player_matches_df.iloc[i:i + games_per_block]))
+        
+    agg_df = pd.DataFrame(rows)
+    
+    #print(agg_df.head())
 
-def get_position_players_stats_df(understat: UnderstatClient, position: List[str],
+    # Set index to date
+    if not agg_df.empty: # Below will error for empty df
+        agg_df = agg_df.set_index("date")
+        
+    return agg_df
+
+def get_position_players_stats_df(understat: UnderstatClient, position: List[str], games_per_block: int,
                       stats: List[str]) -> pd.DataFrame:
     """
     Produces a dataframe of all players with a given position
-    with per-90 stats for every game played by each of the given
+    with aggregate per-90 stats, for every block of games_per_block games played by each of the given
     player_ids, for any club or season played. Indexes by player_id and date.
     """
     
@@ -62,7 +85,7 @@ def get_position_players_stats_df(understat: UnderstatClient, position: List[str
     
     player_ids = get_player_ids(understat, set(position))
     for player_id in player_ids:
-        stats_df = get_player_stats_df(understat, player_id, stats)
+        stats_df = get_player_stats_df(understat, player_id, games_per_block, stats)
         # Add id to index
         stats_df["player_id"] = player_id
         stats_df = stats_df.set_index("player_id", append=True)
@@ -78,14 +101,14 @@ class CustomFootballDataset(Dataset):
     model.
     
     At a given index, one can get a pairing containing metrics for the last 
-    window_size games, along with the current game's metrics as the label.
+    blocks_per_input game blocks, along with the current game's metrics as the label.
     """
     
-    def __init__(self, stats_df: pd.DataFrame, window_size: int = 10, multiple_players: bool = True):
+    def __init__(self, stats_df: pd.DataFrame, blocks_per_input: int = 10, multiple_players: bool = True):
         """
         Initializes a CustomFootballDataset over the given stats_df, storing model
         inputs and outputs in X and y respectively. Each value in X provides
-        a 2d array of stats for the last window_size games, while each value
+        a 2d array of stats for the last blocks_per_input game blocks, while each value
         in y provides stats for the current game to predict with the matching
         X values.
         """
@@ -98,15 +121,15 @@ class CustomFootballDataset(Dataset):
             # Break down by player
             for _, player_df in stats_df.groupby("player_id"):
                 vals = player_df.values
-                for i in range(len(vals) - window_size):
-                    self.X.append(vals[i:i + window_size])
-                    self.y.append(vals[i + window_size])
+                for i in range(len(vals) - blocks_per_input):
+                    self.X.append(vals[i:i + blocks_per_input])
+                    self.y.append(vals[i + blocks_per_input])
                     
         else:
             vals = stats_df.values
-            for i in range(len(vals) - window_size):
-                self.X.append(vals[i:i + window_size])
-                self.y.append(vals[i + window_size])
+            for i in range(len(vals) - blocks_per_input):
+                self.X.append(vals[i:i + blocks_per_input])
+                self.y.append(vals[i + blocks_per_input])
 
     def __len__(self):
         return len(self.X)
